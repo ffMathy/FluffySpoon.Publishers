5840ee6463c9bb7ea60845f9d7dabfb9c7eb3294
Mathias Lorenzen <mathias.lorenzen@live.com>
2022-03-12 01:02:07 +0100

    log attempt.

--- src/FluffySpoon.Publisher.DotNet/Helpers/IProjectFileParser.cs
+++ src/FluffySpoon.Publisher.DotNet/Helpers/IProjectFileParser.cs
@@ -4,19 +4,19 @@ namespace FluffySpoon.Publisher.DotNet.Helpers;
 
 public interface IProjectFileParser
 {
-	XElement GetVersionElement(XDocument projectFile);
+	XElement? GetVersionElement(XDocument projectFile);
 	XElement CreateVersionElement(XDocument projectFile);
 
-	XElement GetPackageProjectUrlElement(XDocument projectFile);
+	XElement? GetPackageProjectUrlElement(XDocument projectFile);
 	XElement CreatePackageProjectUrlElement(XDocument projectFile);
 
-	XElement GetDescriptionElement(XDocument projectFile);
+	XElement? GetDescriptionElement(XDocument projectFile);
 	XElement CreateDescriptionElement(XDocument projectFile);
 
-	XElement GetPackageRepositoryUrlElement(XDocument projectFile);
+	XElement? GetPackageRepositoryUrlElement(XDocument projectFile);
 	XElement CreatePackageRepositoryUrlElement(XDocument projectFile);
 
-	XElement GetPackageRepositoryTypeElement(XDocument projectFile);
+	XElement? GetPackageRepositoryTypeElement(XDocument projectFile);
 	XElement CreatePackageRepositoryTypeElement(XDocument projectFile);
 
 	XElement GetOrCreateElement(XDocument projectFile, string property);
